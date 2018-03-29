c6f0345bcf8d0a5ba1a1444a5e5deb52690b7417
tri291997 <32668090+tri291997@users.noreply.github.com>
2018-03-29 21:47:52 +0700

    jhhjh

--- BTLDTDM/src/main/java/com/example/demo/service/UserServiceIn.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.User;
-
-public interface UserServiceIn {
-	public User findByUsernameAndPassword(String username, String password);
-}
--- BTLDTDM/src/main/webapp/static/Js_TrangChu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "header-navigation pull-right font-transform-inherit") {
-        x.className += " responsive";
-    } else {
-        x.className = "header-navigation pull-right font-transform-inherit";
-    }
-}
