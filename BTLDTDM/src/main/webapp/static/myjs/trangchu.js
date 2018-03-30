6f54f19b5aa167e20b67c84f117aca9d4aa8c15a
tri291997 <32668090+tri291997@users.noreply.github.com>
2018-03-31 00:33:42 +0700

    cdfdf

--- BTLDTDM/src/main/webapp/static/myjs/trangchu.js
+++ BTLDTDM/src/main/webapp/static/myjs/trangchu.js
@@ -10,56 +10,29 @@ $(document).ready(function() {
         	var string = "";
         	if(4 <= data.length){
         		for(var i=0; i<4; i++)
-        			if(i == 0){
-        				string += "<div class='row'>"
-	                        +"<div class='col-sm-2 col-md-2 col-xs-2' style='height: 100px;border-top: dashed;" 
-	                    +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                    + "<img src='https://drive.google.com/uc?export=view&id="+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
+        				string += "<div class='one-news'>"
+	                        +" <div class='news-img'>"+
+                               "<a href='tintucdetail?id="+data[i].idtintuc+"'>" 	                    
+	                            +"<img src='https://drive.google.com/uc?export=view&id="+data[i].linkanh+"' class='NewsImg' style='padding: 5%' />"
+	                        +"</a></div>"
+	                        +"<div class='news-view'>"	          
+	                            +" <div class='tieude_tintuc'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></div>"
+	                        + " <div class='news-day'>"+data[i].thoigian+"</div>"
 	                        +"</div>"
-	                        +"<div class='col-sm-10 col-md-10 col-xs-10' style='height: 100px; width: 80%;border-top: dashed;"
-	                     +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<h4 style='color: black;'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></h4>"
-	                        +"</div>"
-	                    +"</div>";
-        			}
-        			else{
-        				string += "<div class='row'>"
-	                        +"<div class='col-sm-2 col-md-2 col-xs-2' style='height: 100px;" 
-	                    +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<img src='https://drive.google.com/uc?export=view&id="+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
-	                        +"</div>"
-	                        +"<div class='col-sm-10 col-md-10 col-xs-10' style='height: 100px; width: 80%;"
-	                     +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<h4 style='color: black;'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></h4>"
-	                        +"</div>"
-	                    +"</div>";
-        			}
+	                        + "<div class='clear'> </div></div>";
         	}else{
         		for(var i=0; i<data.length; i++)
-        			if(i == 0){
-        				string += "<div class='row'>"
-	                        +"<div class='col-sm-2 col-md-2 col-xs-2' style='height: 100px;border-top: dashed;" 
-	                    +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<img src='"+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
+        			
+        				string += "<div class='one-news'>"
+	                        +" <div class='news-img'>"+
+                               "<a href='tintucdetail?id="+data[i].idtintuc+"'>" 	                    
+	                            +"<img src='https://drive.google.com/uc?export=view&id="+data[i].linkanh+"' class='NewsImg' style='padding: 5%' />"
+	                        +"</a></div>"
+	                        +"<div class='news-view'>"	          
+	                            +" <div class='tieude_tintuc'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></div>"
+	                        + " <div class='news-day'>"+data[i].thoigian+"</div>"
 	                        +"</div>"
-	                        +"<div class='col-sm-10 col-md-10 col-xs-10' style='height: 100px; width: 80%;border-top: dashed;"
-	                     +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<h4 style='color: black;'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></h4>"
-	                        +"</div>"
-	                    +"</div>";
-        			}
-        			else{
-        				string += "<div class='row'>"
-	                        +"<div class='col-sm-2 col-md-2 col-xs-2' style='height: 100px;" 
-	                    +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<img src='"+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
-	                        +"</div>"
-	                        +"<div class='col-sm-10 col-md-10 col-xs-10' style='height: 100px; width: 80%;"
-	                     +"border-bottom: dashed;border-color: #ABABAB; border-width: 1px'>"
-	                            +"<h4 style='color: black;'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></h4>"
-	                        +"</div>"
-	                    +"</div>";
-        			}
+	                        + "<div class='clear'> </div></div>";
         	}
         	
         	$(".cont-tintuc").append(string);
@@ -88,7 +61,7 @@ $(document).ready(function() {
             			string += "<div class='one-news'>"
 	                        +" <div class='news-img'>"+
                                "<a href='tintucdetail?id="+data[i].idtintuc+"'>" 	                    
-	                            +"<img src='https://drive.google.com/uc?export=view&id="+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
+	                            +"<img src='https://drive.google.com/uc?export=view&id="+data[i].linkanh+"' class='NewsImg' style='padding: 5%' />"
 	                        +"</a></div>"
 	                        +"<div class='news-view'>"	          
 	                            +" <div class='tieude_tintuc'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></div>"
@@ -100,7 +73,7 @@ $(document).ready(function() {
             			string += " <div class='one-news'>"
 	                        +" <div class='news-img'>"+
                                "<a href='tintucdetail?id="+data[i].idtintuc+"'>" 	                    
-	                            +"<img src='https://drive.google.com/uc?export=view&id="+ data[i].linkanh +"' class='NewsImg' style='padding: 5%' />"
+	                            +"<img src='https://drive.google.com/uc?export=view&id="+data[i].linkanh+"' class='NewsImg' style='padding: 5%' />"
 	                        +"</a></div>"
 	                        +"<div class='news-view'>"	          
 	                            +" <div class='tieude_tintuc'><a href='tintucdetail?id="+data[i].idtintuc+"'>"+ data[i].tieude +"</a></div>"
